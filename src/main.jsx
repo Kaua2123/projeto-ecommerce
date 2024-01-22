387503df5f8652462ba9063b4c190dd5bd55ab6d
Kaua2123 <kauantnetsoares@gmail.com>
2024-01-22 10:27:48 -0300

    correção

--- src/main.jsx
+++ src/main.jsx
@@ -14,6 +14,7 @@ import './styles/components/CardUser/cardUser.css';
 import './styles/components/ChartProduct/chartProduct.css';
 import './styles/components/Header/header.css';
 
+import { Provider } from 'react-redux';
 import Home from './pages/Home';
 import Products from './pages/Products';
 import Product from './pages/Product';
@@ -24,16 +25,18 @@ import SignUp from './pages/SignUp';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/product/:id" element={<Product />} />
-        <Route path="/requests" element={<Requests />} />
-        <Route path="/users" element={<Users />} />
-        <Route path="/chart" element={<Chart />} />
-        <Route path="/signUp" element={<SignUp />} />
-      </Routes>
-    </BrowserRouter>
+    <Provider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/products" element={<Products />} />
+          <Route path="/product/:id" element={<Product />} />
+          <Route path="/requests" element={<Requests />} />
+          <Route path="/users" element={<Users />} />
+          <Route path="/chart" element={<Chart />} />
+          <Route path="/signUp" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
 );
