105d14331db2a7f482fc3e5c491bb1585556c40b
Kaua2123 <kauantnetsoares@gmail.com>
2024-01-25 08:26:13 -0300

    seção venda de produts

--- src/pages/Products/index.jsx
+++ src/pages/Products/index.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import Header from '../../components/Header';
 
-import Search from '../../imgs/searchWhite.svg';
+import Search from '../../imgs/search.svg';
 
 import Carousel from '../../components/Carousel';
 
@@ -23,11 +23,35 @@ function Products() {
         </div>
       </section>
       <section className="white-bg">
-        <div className="most-buyed-container">
+        <div className="most-buyed-container main-content">
           <h2>Produtos</h2>
 
           <Carousel />
 
+        </div>
+      </section>
+      <section className="minor-section">
+        <div className="sell-products">
+          <h2>Venda de produtos</h2>
+          <div className="sell-products-grid">
+            <div className="sell-products-image-container">
+              <img src={Search} alt="" />
+            </div>
+            <div className="sell-products-inputs">
+              <label className="label-flex" htmlFor="">Nome</label>
+              <input type="text" />
+              <label className="label-flex" htmlFor="">Preço</label>
+              <input type="text" />
+              <label className="label-flex" htmlFor="">Estoque</label>
+              <input type="text" />
+              <label className="label-flex" htmlFor="">Descrição</label>
+              <input type="text" />
+
+              <button type="button" className="brown-btn">
+                Vender
+              </button>
+            </div>
+          </div>
         </div>
       </section>
     </div>
