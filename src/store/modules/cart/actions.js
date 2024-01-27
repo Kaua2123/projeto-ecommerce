1693fbac6c78cac1263e67c7a8921ae3129c7445
Kaua2123 <kauantnetsoares@gmail.com>
2024-01-27 10:06:06 -0300

    redux - configurando actions para o carrinho de compras

---
+++ src/store/modules/cart/actions.js
@@ -0,0 +1,29 @@
+import * as types from '../types';
+
+export function addToCart(payload) {
+  return {
+    type: types.ADD_TO_CART,
+    payload,
+  };
+}
+
+export function removeFromCart(payload) {
+  return {
+    type: types.REMOVE_FROM_CART,
+    payload,
+  };
+}
+
+export function increaseProductQuantity(payload) {
+  return {
+    type: types.INCREASE_PRODUCT_QUANTITY,
+    payload,
+  };
+}
+
+export function decreaseProductQuantity(payload) {
+  return {
+    type: types.DECREASE_PRODUCT_QUANTITY,
+    payload,
+  };
+}
